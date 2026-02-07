7ffb49d6e4e0d39f69454a90649ff420847b8d3b
Xalamor <kornevaleks0705@gmail.com>
2026-02-08 00:51:45 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import App from "./App";
-import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
-      </ThemeProvider>
     </HashRouter>
   </React.StrictMode>
 );
