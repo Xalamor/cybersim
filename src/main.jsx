427d65f59e0eed5df74b8c817a19aed4fbab22b6
Xalamor <kornevaleks0705@gmail.com>
2026-02-08 00:50:46 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import App from "./App.jsx";
-import "./index.css";
-
-const theme = createTheme({
-  palette: {
-    mode: "light",
-    primary: { main: "#1976d2" },
-    secondary: { main: "#9c27b0" },
-  },
-  shape: { borderRadius: 14 },
-  typography: {
-    fontFamily: ["Inter", "system-ui", "Segoe UI", "Roboto", "Arial", "sans-serif"].join(","),
-  },
-});
+import { HashRouter } from "react-router-dom";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import App from "./App";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
